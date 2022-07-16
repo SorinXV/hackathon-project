4dd7853acd5dd26b707440b5e9e81e6cf73483d0
Sorin <sorinmartinescu06@gmail.com>
2022-07-16 15:31:37 +0200

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,12 @@ export default function Navbar(){
     return(
         <>
         <div className='nav'>
-            <img className='test' src={require('../images/logo1.png')} />
-            <a className='button' href='#'>
-                Get bot
-            </a>
+            <img src={require('../images/logo1.png')} />
+            <div className='test1'>
+                <a className='button'>
+                    Try it out now
+                </a>
+            </div>
         </div>
         
        
