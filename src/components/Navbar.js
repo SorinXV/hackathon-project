5992f42fad5812b057d5e6a4bb9b3f1a08db9b26
Sorin <sorinmartinescu06@gmail.com>
2022-07-16 15:19:07 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import navbarcss from '../style/navbar.css'
 import indexcss from '../index.css'
+import logo1 from '../images/logo1.png'
+
 export default function Navbar(){
 
     return(
         <>
+        <div className='nav'>
+            <img className='test' src={require('../images/logo1.png')} />
+            <a className='button' href='#'>
+                Get bot
+            </a>
+        </div>
         
+       
         </>
     )
 }
