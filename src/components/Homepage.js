826011d1b99ebcbfda5d27a134b6746ecf3a83ab
Sorin <sorinmartinescu06@gmail.com>
2022-07-17 07:37:35 +0200

    responsive updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,7 +5,7 @@ export default function Homepage(){
 
         <>
         
-        <div>
+        <div className='test'>
        
             <div className='container'>
                 <h1 className='title '>
